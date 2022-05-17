f37e04997762f2f9383150666b846bfe8b2e1e85
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2022-05-18 01:50:20 +0300

    manzurova gallery

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var hamMenu = document.querySelector('.ham-menu');
-var dropMenu = document.querySelector('.drop-menu');
-var dropMenuList = document.querySelectorAll('.drop-menu div');
-var dropMenuListText = document.querySelectorAll('.drop-menu div a');
+var hamMenu = document.querySelector(".ham-menu");
+var dropMenu = document.querySelector(".drop-menu");
+var dropMenuList = document.querySelectorAll(".drop-menu div");
+var dropMenuListText = document.querySelectorAll(".drop-menu div a");
 
-hamMenu.addEventListener('click', () => {
-    dropMenu.classList.toggle('drop-down');
-    hamMenu.children[0].classList.toggle('top');
-    hamMenu.children[1].classList.toggle('mid');
-    hamMenu.children[2].classList.toggle('bottom');
-})
+hamMenu.addEventListener("click", () => {
+  dropMenu.classList.toggle("drop-down");
+  hamMenu.children[0].classList.toggle("top");
+  hamMenu.children[1].classList.toggle("mid");
+  hamMenu.children[2].classList.toggle("bottom");
+});
+
+$(function () {
+  $(".am-container").montage({
+    liquid: false,
+    fillLastRow: true,
+    margin: 4,
+    fixedHeight: 160,
+  });
+});
