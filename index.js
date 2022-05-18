7cb03f077eec7d289e0bead38f156315a71b8b88
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2022-05-18 18:15:34 +0300

    галерея манцурова

--- index.js
+++ index.js
@@ -10,11 +10,47 @@ hamMenu.addEventListener("click", () => {
   hamMenu.children[2].classList.toggle("bottom");
 });
 
-$(function () {
-  $(".am-container").montage({
-    liquid: false,
-    fillLastRow: true,
-    margin: 4,
-    fixedHeight: 160,
+var $pswp = $(".pswp")[0],
+  galleries = {},
+  photoSwipe;
+$("[data-gallery]").each(function () {
+  var $this = $(this);
+  var galleryName = $this.data("gallery");
+  var bigSize = ($this.data("size") || "800x600").split("x");
+  var gallery = galleries[galleryName] || (galleries[galleryName] = []);
+  var thumb = $this.find("img")[0];
+  var index =
+    gallery.push({
+      w: bigSize[0],
+      h: bigSize[1],
+      src: $this.attr("href"),
+      msrc: thumb.src,
+      title: $this.data("title") || $this.attr("title"),
+    }) - 1;
+  $this.on("click", function (event) {
+    event.preventDefault();
+    photoSwipe = new PhotoSwipe($pswp, PhotoSwipeUI_Default, gallery, {
+      getThumbBoundsFn: nodeBounder(thumb),
+      galleryUID: galleryName,
+      index: index,
+      bgOpacity: 0.7,
+    });
+    photoSwipe.init();
   });
+  // prevent the famous hoverZoom browser plugin form ruining the experience
+  setTimeout(function () {
+    $this.removeClass("hoverZoomLink");
+  }, 100);
 });
+var nodeBounder = function (node) {
+  return function (i) {
+    //don't need the index, we have the thumb node itself (as a closure variable)
+    var rect = node.getBoundingClientRect();
+    return {
+      x: rect.left,
+      w: rect.width,
+      h: rect.height,
+      y: rect.top + document.documentElement.scrollTop,
+    };
+  };
+};
